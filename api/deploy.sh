f3b4f8fa13b4a653937e4e5fd355f8860f96bbf3
Ahyeong <aylee618@gmail.com>
2021-04-15 15:28:41 +0900

    add deploy.sh

---
+++ api/deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PID=$(ps -ef | grep 'node api.js' | grep -v grep | awk '{print \$2}')
+if [[ ${PID} -eq "" ]]
+then
+    echo "none"
+else
+    kill -9 ${PID}    
+fi
+cd /home/jenkins/deploy
+npm install
+node api.js > /dev/null 2> /dev/null < /dev/null &
+EOF
